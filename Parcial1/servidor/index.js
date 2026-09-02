438763773d69d313312d064f4c206d02607d74d4
Anapaula-Ren <L23100201@nlaredo.tecnm.mx>
2026-09-02 09:02:37 -0500

    middleware

--- Parcial1/servidor/index.js
+++ Parcial1/servidor/index.js
@@ -1,10 +1,27 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 const PORT = 3000;
 
 
 app.use(express.json());
 
+app.use(morgan('dev'));
+
+const validarRutaPermitida = (req, res, next) => {
+    const ruta = req.url;
+
+    // Permitimos la ruta raíz '/' o cualquier ruta que empiece con '/saludo'
+    if (ruta === '/' || ruta.startsWith('/saludo')) {
+        return next(); // Todo bien, pase usted
+    }
+
+    // Si escribieron cualquier otra cosa (como /pene, /basura, /loquesea), bloqueamos
+    res.status(403).send('🛑 ¡Acceso denegado! Palabra no permitida en la ruta.');
+};
+
+// Activamos el middleware para que filtre todo lo que entre
+app.use(validarRutaPermitida);
 const miRuta= require('./routes/routes'); 
 
 app.use ('/', miRuta); 
--- Parcial1/servidor/routes/routes.js
+++ Parcial1/servidor/routes/routes.js
@@ -12,4 +12,15 @@ router.get('/saludo/:nombre', (req, res) => {
     res.send(`¡Hola, ${nombreUsuario}! Que tengas un excelente día.`);
 });
 
+router.post('/saludo', (req, res) => {
+    // Extraemos los datos que vienen en el cuerpo (body) de la petición
+    const { nombre, mensaje } = req.body;
+
+    res.json({
+        estado: '¡Éxito! ',
+        respuesta: `Hola ${nombre}, tu mensaje fue recibido correctamente.`,
+        tuMensaje: mensaje
+    });
+});
+
 module.exports = router;
