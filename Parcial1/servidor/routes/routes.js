ae69cd67327d2a1a6ebfd1288d3b24ad2280f5d8
Anapaula-Ren <L23100201@nlaredo.tecnm.mx>
2026-09-09 08:50:41 -0500

    feat: visats pug

--- Parcial1/servidor/routes/routes.js
+++ Parcial1/servidor/routes/routes.js
@@ -6,6 +6,14 @@ router.get('/', (req, res) => {
     res.send('Hola escribe "/saludo/tu nombre" pa darte un saludin especial');
 });
 
+router.get('/saludo/:nombre/pug', (req, res) => {
+    const nombreUsuario = req.params.nombre;
+
+        res.render('index', {
+        titulo: 'Saludo Especial',
+        mensaje: `¡Hola, ${nombreUsuario}! Que tengas un Pugcelente día.`
+    });
+});
 
 router.get('/saludo/:nombre', (req, res) => {
     const nombreUsuario = req.params.nombre;
